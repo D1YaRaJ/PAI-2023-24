ea38fb35ec186b934ec76ab1c2729f5a63faa3f1
Diya Raj <156573305+D1YaRaJ@users.noreply.github.com>
2024-05-06 23:32:57 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,15 @@
+#datatypes
+x="hi"
+y=20.5
+z=20
+w=1j
+a=["bmw","swift","audi","bmw"]#list
+b=("bmw","swift","audi","bmw")#tuple
+c={"bmw","swift","audi","bmw"}#set
+d={"name":"swift","model":"swift dzire","fuel":"diesel","year":"2023"}#dictionary
+print(x,y,z,w)
+print(y+z+w)
+print(a,b,c,d)
+print(d["name"],d["fuel"])
+print(d.keys())
+print(d.values())
