cd7897f3c6cabc112210f339cdc2d3e5b4934264
Diya Raj <156573305+D1YaRaJ@users.noreply.github.com>
2024-05-06 22:46:12 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,17 @@
+x="Hello"
+y='World'
+print(x+y)
+print(type(x))
+#single line comment
+"""
+multi
+line
+comment
+"""
+print("hello"+" world!!")
+a=int(input("enter 1st value:"))
+b=int(input("enter 2nd value:"))
+print(a+b)
+l,m,n=6,7,2
+print(str(l)+str(m)+str(n))
+print(l+m+n)
