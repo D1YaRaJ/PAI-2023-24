f36477d2bbd1cd83aa09debe10847a152d49a2e7
Diya Raj <156573305+D1YaRaJ@users.noreply.github.com>
2024-06-15 21:55:11 +0530

    evenodd.py

--- evenodd.py
+++ evenodd.py
@@ -1,5 +1,13 @@
+#even odd
 n=int(input("Enter a number:"))
 if n%2==0:
     print(n,"is even")
 else:
     print(n,"is odd")
+
+#positive negative
+n=int(input("Enter a number:"))
+if n<0:
+    print(n,"is negative")
+else:
+    print(n,"is positive")
