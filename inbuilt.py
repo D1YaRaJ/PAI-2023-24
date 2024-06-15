25f4ad9e142c14f5b6cd99b04d3c27c5e6a88a4a
Diya Raj <156573305+D1YaRaJ@users.noreply.github.com>
2024-06-15 22:32:16 +0530

    Add files via upload

---
+++ inbuilt.py
@@ -0,0 +1,14 @@
+x=True
+a=6,9,3,7,2,1
+print(bin(8))
+print(max(3,7,2))
+print(min(3,7,2))
+print(bool(x))
+print(float(3/5))
+print(hex(34))
+print(len("hello"))
+print(pow(2,3))
+print(round(76.567))
+print(sorted(a))
+print(sum(a))
+print(type(x))
