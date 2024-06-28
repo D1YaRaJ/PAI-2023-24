9032e1532743492d8c7b74ae0a76bdf65c3b3eb0
Diya Raj <156573305+D1YaRaJ@users.noreply.github.com>
2024-06-28 07:48:24 +0530

    Delete Array1.py

--- Array1.py
+++
@@ -1,9 +0,0 @@
-array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-max_element = array[0]
-
-for num in array:
-    if num > max_element:
-        max_element = num
-
-print("Array:", array)
-print("Maximum element:", max_element)
